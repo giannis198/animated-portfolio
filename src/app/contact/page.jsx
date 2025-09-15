97903f68f687bc56ab730605102f73a4a362d8c8
htmlhero20 <htmlhero20@gmail.com>
2025-09-15 16:29:18 +0300

    feat:socials updated

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -8,7 +8,7 @@ const ContactPage = () => {
       <div className="h-full flex flex-col lg:flex-row container">
         {/* Text Container */}
         <div className="h-1/5 lg:h-full lg:w-1/2  flex items-center justify-center text-5xl lg:text-6xl">
-          <MotionText text="Say Hello" />
+          <MotionText text="Say Hello!" />
         </div>
         {/* form */}
         <ContactForm />
